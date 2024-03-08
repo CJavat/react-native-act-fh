c0ba8f69a0b4efee0009f7645236346335c931d1
cjavat <cdpm98@hotmail.com>
2024-03-08 16:46:21 -0600

    SECCIÓN 14 - TERMINADO

--- 08-Pokedex/src/actions/pokemons/get-pokemons.ts
+++ 08-Pokedex/src/actions/pokemons/get-pokemons.ts
@@ -7,7 +7,7 @@ import { PokemonMapper } from "../../infrastructure/mappers/pokemon.mapper";
 export const getPokemons = async ( page: number, limit: number = 20 ): Promise<Pokemon[]> => {
 
   try {
-    const url = `/pokemon?offset=${ page }&limit=${ limit }`;
+    const url = `/pokemon?offset=${ page * 10 }&limit=${ limit }`;
     const { data } = await pokeApi.get<PokeAPIPaginatedResponse>( url );
 
     const pokemonPromises = data.results.map( (info) => {
@@ -16,11 +16,10 @@ export const getPokemons = async ( page: number, limit: number = 20 ): Promise<P
 
     const pokeApiPokemons = await Promise.all( pokemonPromises );
 
-    const pokemons = pokeApiPokemons.map( (item) => PokemonMapper.pokeApiPokemonToEntity( item.data ) );
+    const pokemonsPromises = pokeApiPokemons.map( (item) => PokemonMapper.pokeApiPokemonToEntity( item.data ) );
 
-    console.log( JSON.stringify(pokemons[0], null, 2) );
 
-    return pokemons;
+    return await Promise.all( pokemonsPromises );
   } catch (error) {
     console.log( error );
     throw new Error('Error getting pokemons');
--- Pokedex/src/presentation/screens/pokemon/PokemonScreen.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native"
-
-export const PokemonScreen = () => {
-  return (
-    <View>
-      <Text>PokemonScreen</Text>
-    </View>
-  )
-}
