2e5616167d4e58dfdfe05cc7436a83bafecfe649
cjavat <cdpm98@hotmail.com>
2024-03-09 20:55:01 -0600

    SECCIÓN 18 / CAP 13 - EN CURSO

---
+++ 10-ProductsApp/src/presentation/screens/auth/LoginScreen.tsx
@@ -0,0 +1,72 @@
+import { Button, Input, Layout, Text } from "@ui-kitten/components"
+import { useWindowDimensions } from "react-native"
+import { ScrollView } from "react-native-gesture-handler"
+import { MyIcon } from "../../components/ui/MyIcon";
+import { StackScreenProps } from "@react-navigation/stack";
+import { RootStackParams } from "../../navigation/StackNavigator";
+
+interface Props extends StackScreenProps<RootStackParams, 'LoadingScreen'> {};
+
+export const LoginScreen = ( { navigation }: Props ) => {
+
+  const { height } = useWindowDimensions();
+
+  return (
+    <Layout style={{ flex: 1 }}>
+      <ScrollView style={{ marginHorizontal: 40 }}>
+
+      <Layout style={{ paddingTop: height * 0.35 }}>
+        <Text category="h1">Ingresar</Text>
+        <Text category="p2">Por favor, ingrese para continuar</Text>
+      </Layout>
+
+      {/* Inputs */}
+      <Layout style={{ marginTop: 20 }}>
+        <Input 
+          placeholder="Correo Electrónico"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          accessoryLeft={ <MyIcon name="email-outline" /> }
+          style={{ marginBottom: 10 }}
+        />
+
+        <Input 
+          placeholder="Contraseña"
+          autoCapitalize="none"
+          accessoryLeft={ <MyIcon name="lock-outline" /> }
+          secureTextEntry
+          style={{ marginBottom: 20 }}
+        />
+      </Layout>
+
+      {/* Space */}
+      <Layout style={{ height: 20 }} />
+
+      <Layout>
+        <Button
+          onPress={ () => {} }
+          accessoryRight={ <MyIcon name="arrow-forward-outline" white /> }
+        >
+          Ingresar
+        </Button>
+      </Layout>
+
+      {/* Información para crear cuenta */}
+      <Layout style={{ height: 50 }} />
+
+      <Layout style={{ alignItems: 'flex-end', flexDirection: 'row', justifyContent: 'center' }}>
+        <Text>¿No tienes cuenta?, </Text>
+        <Text 
+          status="primary" 
+          category="s1"
+          onPress={ () => navigation.navigate('RegisterScreen') }
+        > 
+          Crea una cuenta
+        </Text>
+      </Layout>
+
+
+      </ScrollView>
+    </Layout>
+  )
+}
---
+++ 10-ProductsApp/src/presentation/screens/loading/LoadingScreen.tsx
@@ -0,0 +1,19 @@
+import { Layout } from "@ui-kitten/components"
+import { ScrollView } from "react-native-gesture-handler"
+
+export const LoadingScreen = () => {
+
+  
+
+  return (
+    <Layout>
+      <ScrollView>
+
+      
+
+
+
+      </ScrollView>
+    </Layout>
+  )
+}
